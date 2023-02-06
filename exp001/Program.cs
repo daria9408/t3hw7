5e9c234c3b5d8e57763bf805aa0ab996bfb20878
unknown <solnce-94.08@mail.ru>
2023-02-06 19:33:43 +0300

    exp001 run

--- exp001/Program.cs
+++ exp001/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+//шаг 1. ввод данных
+Console.WriteLine("Введите значение m (строки):");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение n (столбцы):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+//шаг 2. задаем массив
+
+double[,] numbers = new double[m, n];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+//шаг 3. заполняем массив
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10));
+        }
+    }
+}
+
+//шаг 4. выводим массив
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
